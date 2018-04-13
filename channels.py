5b35b823b537375e7c87151b179b51b064ca2eda
Dan Bradham <danielbradham@gmail.com>
2018-04-12 20:42:18 -0400

    WIP new channels implementation

--- channels.py
+++ channels.py
@@ -1,57 +1,58 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
-'''
-Is this worth the effort?
-'''
 from collections import defaultdict
 import inspect
 import weakref
 
 
-try:
-    basestring
-except NameError:
-    basestring = str
-
-
-ANY = 'ANY'
-
-
-class reference(weakref.ref):
+class WeakRef(weakref.ref):
     pass
 
 
-class WeakList(object):
-    '''Like WeakList but works with bound methods'''
+class WeakMeth(object):
+
+    def __init__(self, obj, callback=None):
+        self.name = obj.__name__
+        self.ref = WeakRef(obj.__self__, callback)
+
+    def __call__(self):
+        inst = self.ref()
+        if inst is None:
+            return
+        return getattr(inst, self.name)
+
+
+class WeakSet(object):
 
     def __init__(self):
-        self._ids = []
         self._refs = []
-
-    def __repr__(self):
-        return (
-            '<{}>(_ids={}, _refs={})'
-        ).format(
-            self.__class__.__name__,
-            self._ids,
-            self._refs
-        )
+        self._ids = []
 
     def __len__(self):
-        return len(self._refs)
+        return len(self._ids)
+
+    def __contains__(self, obj):
+        return (
+            obj in self._ids or
+            obj in self._refs or
+            self._ref_id(obj) in self._ids
+        )
 
     def __iter__(self):
-        for i in range(len(self._refs)):
-            ref_id = self._ids.pop(0)
-            ref = self._refs.pop(0)
+        for ref in self._refs:
             obj = ref()
-            if obj is not None:
-                yield obj
-                self._refs.append(ref)
-                self._ids.append(ref_id)
+            if obj is None:
+                continue
+            yield obj
+
+    def _ref_id(self, obj):
+        if inspect.ismethod(obj):
+            return (id(obj.__self__), id(obj.__func__))
+        else:
+            return id(obj)
 
     def _cleanup_ref(self, ref):
-        ref_id = ref.reference_id
+        ref_id = ref.ref_id
         if ref_id not in self._ids:
             return
 
@@ -59,236 +60,260 @@ class WeakList(object):
         self._ids.pop(index)
         self._refs.pop(index)
 
-    def remove(self, obj):
-        ref_id = reference_id(obj)
-
-        if ref_id not in self._ids:
-            return
-
-        index = self._ids.index(ref_id)
-        self._ids.pop(index)
-        self._refs.pop(index)
-
-    def append(self, obj):
-        ref_id = reference_id(obj)
-
+    def add(self, obj):
+        ref_id = self._ref_id(obj)
         if ref_id in self._ids:
             return
 
         self._ids.append(ref_id)
-
         if inspect.ismethod(obj):
-            ref = weakmeth(obj, self._cleanup_ref)
-            ref.ref.reference_id = ref_id
+            ref = WeakMeth(obj, self._cleanup_ref)
+            ref.ref.ref_id = ref_id
         else:
-            ref = reference(obj, self._cleanup_ref)
-            ref.reference_id = ref_id
+            ref = WeakRef(obj, self._cleanup_ref)
+            ref.ref_id = ref_id
 
         self._refs.append(ref)
 
-
-class weakmeth(object):
-    '''Like weakref but for bound methods'''
-
-    def __init__(self, meth, callback=None):
-        self.name = meth.__name__
-        if callback:
-            self.ref = reference(meth.__self__, callback)
-        else:
-            self.ref = reference(meth.__self__)
-
-    def __call__(self):
-        obj = self.ref()
-        if obj is None:
+    def discard(self, obj):
+        ref_id = self._ref_id(obj)
+        if ref_id not in self._ids:
             return
-        return getattr(obj, self.name, None)
+
+        index = self._ids.index(ref_id)
+        self._ids.pop(index)
+        self._refs.pop(index)
 
 
-def reference_id(obj):
-    if inspect.ismethod(obj):
-        return (id(obj.__self__), id(obj.__func__))
-    elif isinstance(obj, basestring):
-        return obj
-    else:
-        return id(obj)
+class Dispatcher(object):
+    '''Called by a channel to dispatch a message to a receiver. The default
+    dispatcher simply executes the receiver passing along the args and kwargs
+    passed to Channel.send.
 
-
-class BoundChannel(object):
-    '''Like a standard Channel, but accepts an additional argument, sender.
-    BoundChannels will always pass their sender to all Channel methods.
-
-    Returned by Channel.__get__ when Channel is used as a class attribute.
+    The purpose of the Dispatcher class is to allow users to override the
+    execution behavior of Channel receivers. For example in a Qt application,
+    a user may want to execute the receiver in the main thread, or queue the
+    receiver in the main event loop and await the result.
     '''
 
-    def __init__(self, channel, sender):
-        self.channel = channel
-        self.sender = sender
+    def dispatch(self, receiver, *args, **kwargs):
+        return receiver(*args, **kwargs)
 
-    def __getattr__(self, attr):
-        return getattr(self.channel, attr)
 
-    def __repr__(self):
-        return '<{} at 0x{}>(identifier={!r}, sender={!r})'.format(
-            self.__class__.__name__,
-            id(self),
-            self.identifier,
-            self.sender
-        )
+DEFAULT_DISPATCHER = Dispatcher()
 
-    def get_receivers(self):
-        self.channel.get_receivers(sender=self.sender)
 
-    def send(self, *args, **kwargs):
-        kwargs.setdefault('sender', self.sender)
-        self.channel.send(*args, **kwargs)
+class Band(object):
+    '''A collection of Channels. A Channel in one band, will not receiver
+    signals from another band. You can override the execution behavior of
+    receivers by providing your own Dispatcher with a dispatch method.
 
-    def clear(self):
-        self.channel.clear(self.sender)
+    A band will always return the same Channel instance for a given identifier
+    and parent. However, Band stores only weakrefs to Channels, so if you're
+    channel goes out of scope, it will be deleted and any receivers will be
+    lost.
 
-    def connect(self, obj):
-        self.channel.connect(obj, sender=self.sender)
+    Arguments:
+        dispatcher: Dispatcher object with a dispatch method
+    '''
 
-    def disconnect(self, obj):
-        self.channel.disconnect(obj, sender=self.sender)
+    def __init__(self, dispatcher=None):
+        self.dispatcher = dispatcher or DEFAULT_DISPATCHER
+        self.channels = {}
+        self.by_parent = defaultdict(dict)
+        self.by_identifier = defaultdict(set)
+
+    def _cleanup_channel(self, ref):
+        '''Cleanup a channel after it's reference dies'''
+
+        identifier, parent_id = ref.key
+        self.by_identifier[identifier].discard(ref.key)
+        self.by_parent[parent_id].pop(identifier, None)
+        self.channels.pop(ref.key, None)
+
+    def dispatch(self, receiver, *args, **kwargs):
+        '''Executes a receiver using this Band's dispatcher'''
+
+        return self.dispatcher.dispatch(receiver, *args, **kwargs)
+
+    def get_channel_receivers(self, chan):
+        '''Get all receivers for the provided channel.
+
+        If the channel is bound, yield the bound Channel's receivers
+        plus any anonymous receivers connected to an unbound Channel with the
+        same identifier.
+
+        If the channel is unbound, yield receivers connected to all unbound
+        and bound Channels with the same identifier.
+        '''
+
+        if chan.bound:
+            yield chan.receivers
+            key = chan.identifier, id(None)
+            if key in self.channels:
+                any_chan = self.channels[(chan.identifier, id(None))]()
+                if any_chan:
+                    yield any_chan.receivers
+        else:
+            yield chan.receivers
+            for key in self.by_identifier[chan.identifier]:
+                other_chan = self.channels[key]()
+                if other_chan is chan:
+                    continue
+                yield other_chan.receivers
+
+    def channel(self, identifier, parent=None):
+        '''Get a Channel instance for the provided identifier. If a parent is
+        provided return a bound Channel, otherwise return an unbound Channel
+
+        Arguments:
+            identifier (str): Identifier of Channel like "started"
+            parent (obj): Parent to bind Channel to
+
+        Returns:
+            unbound or bound Channel
+        '''
+
+        key = (identifier, id(parent))
+        if key not in self.channels:
+            chan = Channel(identifier, parent, self)
+            ref = WeakRef(chan, self._cleanup_channel)
+            ref.key = key
+            self.channels[key] = ref
+            self.by_parent[id(parent)][identifier] = key
+            self.by_identifier[identifier].add(key)
+        return self.channels[key]()
+
+
+DEFAULT_BAND = Band()
+ACTIVE_BAND = DEFAULT_BAND
+
+
+def get_band():
+    '''Get the currently active Band'''
+
+    return ACTIVE_BAND
+
+
+def use_default_band():
+    '''Set the active band to the default band'''
+
+    global ACTIVE_BAND
+    ACTIVE_BAND = DEFAULT_BAND
+
+
+def use_band(band):
+    '''Set the active band to the provided band'''
+
+    global ACTIVE_BAND
+    ACTIVE_BAND = band
+
+
+def channel(identifier, parent=None, band=None):
+    '''Get a Channel instance for the provided identifier in the active band.
+    If a parent is provided return a bound Channel, otherwise return an
+    unbound Channel.
+
+    Arguments:
+        identifier (str): Identifier of Channel like "started"
+        parent (obj): Parent to bind Channel to
+
+    Returns:
+        unbound or bound Channel
+    '''
+
+    band = band or ACTIVE_BAND
+    return band.channel(identifier, parent)
+
+
+def send(identifier, *args, **kwargs):
+    '''Send a message to a Channel with the given identifier and parent in
+    the active band. If no parent is provided, broadcasts *args and **kwargs
+    to all unbound and bound receivers for identifier.
+
+    Arguments:
+        identifier (str): Identifier of Channel like "started"
+        parent (obj): Parent to bind Channel to
+
+    Returns:
+        list of results
+    '''
+
+    parent = kwargs.pop('parent', None)
+    band = kwargs.pop('parent', ACTIVE_BAND)
+    return band.channel(identifier, parent).send(*args, **kwargs)
 
 
 class Channel(object):
+    '''A Channel which users can use to send messages to connected receivers.
 
-    _channels = weakref.WeakValueDictionary()
+    In literal terms, a Channel is a registry of functions that get called
+    in the order they were connected to a Channel instance. Typically users
+    do not create Channel instances manually, they use the :func:`channel` to
+    create Channels in the active Band or use :meth:`Band.channel` to
+    explicitly create a Channel in a Band.
 
-    def __new__(cls, identifier):
-        if identifier not in cls._channels:
-            inst = super(Channel, cls).__new__(cls, identifier)
-            cls._channels[identifier] = inst
-        return cls._channels[identifier]
+    A Channel can be unbound (anonymous) or bound to an object. When messages
+    are sent through an unbound Channel, they are broadcast to unbound and
+    bound receivers for the Channel's identifier. When sent through a bound
+    Channel, messages are sent only to the bound Channel's receivers and any
+    receivers connected to the unbound Channel with the same identifier.
 
-    def __init__(self, identifier):
+    Channel objects are also descriptors. When used as a class attribute, the
+    Channel will become bound upon first access.
+    '''
+
+    def __init__(self, identifier, parent=None, band=None):
         self.identifier = identifier
-        self._receivers = defaultdict(WeakList)
-        self._senders = {}
+        if parent:
+            self.parent = weakref.proxy(parent)
+        else:
+            self.parent = parent
+        self.band = band or get_band()
+        self.receivers = WeakSet()
 
-    def __repr__(self):
-        return '<{} at 0x{}>(identifier={!r}, sender={!r})'.format(
-            self.__class__.__name__,
-            id(self),
-            self.identifier,
-            ANY
-        )
-
-    def __get__(self, obj=None, type=None):
-        '''When channels are assigned as class attributes, accessing them
-        through a class attribute returns a BoundChannel. A BoundChannel is
-        the same as a standard Channel, except that the sender is always set
-        as the class instance.
-        '''
-
+    def __get__(self, obj, type):
         if obj is None:
             return self
-        return BoundChannel(self, obj)
 
-    def get_receivers(self, sender=None):
-        '''Get receivers for the specified sender. If no sender is provided,
-        get all receivers in the chanenl.
-        '''
+        chan = self.band.channel(self.identifier, obj)
 
-        sender = sender or ANY
-        sender_id = reference_id(sender)
+        # Bind this descriptor to the class instance
+        for name, member in inspect.getmembers(type):
+            if member is self:
+                setattr(obj, name, chan)
 
-        if sender is ANY:
-            all_receivers = self._receivers.values()
-            if all_receivers:
-                return [r for receivers in all_receivers for r in receivers]
-            else:
-                return all_receivers
+        return chan
 
-        return list(self._receivers[sender_id]) + list(self._receivers[ANY])
+    def __repr__(self):
+        return '<{} {} at 0x{}>(identifier={!r})'.format(
+            ('unbound', 'bound')[self.bound],
+            self.__class__.__name__,
+            id(self),
+            self.identifier
+        )
+
+    def get_receivers(self):
+        for receivers in self.band.get_channel_receivers(self):
+            for receiver in receivers:
+                yield receiver
+
+    @property
+    def bound(self):
+        return self.parent is not None
 
     def send(self, *args, **kwargs):
-        '''Send a message to all receivers in the channel. The default
-        behavior is to send a message to receivers of ANY sender. You may send
-        messages along to receivers of just one sender by specifying the sender
-        as a keyword argument.
-        '''
-
-        sender = kwargs.pop('sender', ANY)
         return [
-            receiver(sender, *args, **kwargs)
-            for receiver in self.get_receivers(sender)
+            self.band.dispatch(receiver, *args, **kwargs)
+            for receiver in self.get_receivers()
         ]
 
-    def clear(self, sender=None):
-        '''Clear all receivers in the channel. The default behavior is to
-        clear all receivers. You may pass a sender if you'd like to clear
-        receivers for just one sender.
-        '''
+    def connect(self, obj):
+        self.receivers.add(obj)
 
-        sender = sender or ANY
-        sender_id = reference_id(sender)
-
-        if sender is ANY:
-            self._receivers = defaultdict(set)
-        else:
-            self._receivers.pop(sender_id, None)
-            self._senders.pop(sender_id, None)
-
-    def connect(self, obj, sender=None):
-        '''Set an obj to receive messages from this channel. The default
-        behavior is to connect the obj to receive messages from ANY sender.
-        You may pass a sender if you'd like to have the obj receive messages
-        from just one sender.
-        '''
-
-        sender = sender or ANY
-        sender_id = reference_id(sender)
-
-        if sender is not ANY and sender_id not in self._senders:
-            ref = reference(sender, self._cleanup_sender)
-            ref.reference_id = sender_id
-            self._senders[sender_id] = ref
-
-        self._receivers[sender_id].append(obj)
-
-    def disconnect(self, obj, sender=None):
-        '''Disconnect an obj from the channel. The default behavior is to
-        remove the obj from all senders. You may pass a sender if you'd like
-        to stop the obj from receiving from just that sender.
-        '''
-
-        sender = sender or ANY
-        sender_id = reference_id(sender)
-
-        if sender is not ANY and sender_id in self._receivers:
-            self._receivers[sender_id].remove(obj)
-        else:
-            for receivers in self._receivers.values():
-                receivers.remove(obj)
-
-    def _cleanup_sender(self, ref):
-        '''When a sender dies disconnect it's receivers'''
-
-        self._receivers.pop(ref.reference_id, None)
-        self._senders.pop(ref.reference_id, None)
+    def disconnect(self, obj):
+        self.receivers.discard(obj)
 
 
-def receiver(sender, *args, **kwargs):
-    print('r', sender, args, kwargs)
-
-
-def bound_receiver(sender, *args, **kwargs):
-    print('br', sender, args, kwargs)
-
-
-class Object(object):
-
-    chan = Channel('channel-1')
-
-    def __init__(self):
-        self.chan.connect(self.o_bound_receiver)
-
-    def o_bound_receiver(self, sender, *args, **kwargs):
-        print('obr', sender, args, kwargs)
-
-
-Object.chan.connect(receiver)
-Object().chan.send(100)
-Object.chan.send(200)
+def is_channel(obj):
+    return isinstance(obj, Channel)
